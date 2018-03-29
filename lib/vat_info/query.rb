7a5147b6744d05bad5b33f3a791df3d516fbfad0
landovsky <landovsky@gmail.com>
2018-03-29 08:55:36 +0200

    rescue MFCR sorry-page response

--- lib/vat_info/query.rb
+++ lib/vat_info/query.rb
@@ -4,9 +4,11 @@ module VatInfo
   class SchemaError < StandardError; end
 
   class Query
-    DOCS = 'https://adisspr.mfcr.cz/adistc/adis/idpr_pub/dpr_info/ws_spdph.faces'
+    DOCS = 'https://adisspr.mfcr.cz/adistc/adis/idpr_pub/dpr_info/ws_spdph.faces'.freeze
     WSDL = 'http://adisrws.mfcr.cz/adistc/axis2/services/rozhraniCRPDPH.rozhraniCRPDPHSOAP?wsdl'.freeze
-    TIMEOUT = 2
+    TIMEOUT             = 2
+    SERVICE_UNAVAILABLE = 503
+    REQUEST_TIME_OUT    = 408
 
     def self.call(request, endpoint, wsdl = WSDL, timeout = TIMEOUT)
       client = Savon.client(wsdl: wsdl, open_timeout: timeout)
@@ -16,10 +18,16 @@ module VatInfo
         if response.success?
           VatInfo::Response.new(status_code: 200, raw: response.body)
         else
-          VatInfo::Response.new(status_code: 503)
+          VatInfo::Response.new(status_code: SERVICE_UNAVAILABLE)
+        end
+      rescue Savon::HTTPError => e
+        if e.to_s.include?('sorry-page.html')
+          VatInfo::Response.new(status_code: SERVICE_UNAVAILABLE)
+        else
+          raise e
         end
       rescue Net::OpenTimeout
-        VatInfo::Response.new(status_code: 408)
+        VatInfo::Response.new(status_code: REQUEST_TIME_OUT)
       rescue Savon::SOAPFault => e
         raise SchemaError, 'The SOAP schema of VAT service may have changed. Go to '\
                            "#{DOCS} to verify. Original error: #{e}"
